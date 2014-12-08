49edff2f2aa84d7aee5bab8b99d2ca55adfea0cf
Leonardo1994 <leonardo.teixeira.dos@gmail.com>
2014-12-08 21:38:19 -0200

    Initial Commit

---
+++ CyberPizza.LojaVirtual.UnitTest/UnitTestCyberPizza.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CyberPizza.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTestCyberPizza
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
