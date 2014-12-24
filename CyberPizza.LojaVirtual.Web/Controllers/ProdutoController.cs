1ba5675012fa7e1f3ea9f77c7ac23675c69f2e96
Leonardo <leonardo.teixeira.dos@gmail.com>
2014-12-24 00:47:58 -0200

    Controleer and Views with EntityFramework

---
+++ CyberPizza.LojaVirtual.Web/Controllers/ProdutoController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CyberPizza.LojaVirtual.Dominio.Repositorio;
+
+namespace CyberPizza.LojaVirtual.Web.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private ProdutosRepositorio _repositorio;
+
+
+        // GET: Produto
+        public ActionResult Index()
+        {
+            _repositorio = new ProdutosRepositorio();
+            var produtos = _repositorio.Produtos.Take(2);
+            return View(produtos);
+        }
+    }
+}
