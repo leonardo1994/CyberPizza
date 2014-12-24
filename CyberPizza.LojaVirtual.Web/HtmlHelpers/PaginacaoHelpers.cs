52c193534a273e775f3471c88eafb4031cafc6b0
Leonardo <leonardo.teixeira.dos@gmail.com>
2014-12-24 12:30:14 -0200

    Class The Pagination

---
+++ CyberPizza.LojaVirtual.Web/HtmlHelpers/PaginacaoHelpers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using CyberPizza.LojaVirtual.Web.Models;
+
+namespace CyberPizza.LojaVirtual.Web.HtmlHelpers
+{
+    public static class PaginacaoHelpers
+    {
+        public static MvcHtmlString PageLinks(this HtmlHelper htmlHelper, Paginacao paginacao, Func<int, string> paginaUrl)
+        {
+           var resultado = new StringBuilder();
+            for (var i = 1; i < paginacao.TotalPagina; i++)
+            {
+                var tag = new TagBuilder("a");
+                tag.MergeAttribute("href", paginaUrl(i));
+                tag.InnerHtml = i.ToString();
+
+                if (i == paginacao.PaginaAtual) tag.AddCssClass("Selected"); tag.AddCssClass("btn-primary");
+                tag.AddCssClass("btn-default");
+                resultado.Append(tag);
+            }
+
+            return MvcHtmlString.Create(resultado.ToString());
+        }
+    }
+}
