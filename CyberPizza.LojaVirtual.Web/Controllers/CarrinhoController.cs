63d1288ffccb82a0036b1cb8abd63ce6e5786e82
Leonardo <leonardo.teixeira.dos@gmail.com>
2014-12-31 02:14:09 -0200

    Shopping Car Implements

---
+++ CyberPizza.LojaVirtual.Web/Controllers/CarrinhoController.cs
@@ -0,0 +1,65 @@
+﻿using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Web.Mvc;
+using System.Web.Routing;
+using CyberPizza.LojaVirtual.Dominio.Entidades;
+using CyberPizza.LojaVirtual.Dominio.Repositorio;
+using CyberPizza.LojaVirtual.Web.Models;
+
+namespace CyberPizza.LojaVirtual.Web.Controllers
+{
+    public class CarrinhoController : Controller
+    {
+        private ProdutosRepositorio _repositorio;
+
+        //Adicionar Produto
+        public RedirectToRouteResult Adicionar(int produtoId, string returnUrl)
+        {
+            _repositorio = new ProdutosRepositorio();
+
+            Produto produto = _repositorio.Produtos.FirstOrDefault(p => p.ProdutoId == produtoId);
+
+            if (produto != null)
+            {
+                ObterCarrinho().AdicionarItem(produto, 1);
+            }
+
+            return RedirectToAction("Index", new {returnUrl});
+        }
+
+        // Obter Carrinho
+        private Carrinho ObterCarrinho()
+        {
+            var carrinho = (Carrinho) Session["Carrinho"];
+            if (carrinho != null) return carrinho;
+            carrinho = new Carrinho();
+            Session["Carrinho"] = carrinho;
+            return carrinho;
+        }
+
+        //Remover do Carrinho
+        public RedirectToRouteResult Remover(int produtoId, string returnUrl)
+        {
+            _repositorio = new ProdutosRepositorio();
+
+            Produto produto = _repositorio.Produtos
+                .FirstOrDefault(p => p.ProdutoId == produtoId);
+
+            if (produto != null)
+            {
+                ObterCarrinho().RemoverItem(produto);
+            }
+
+            return RedirectToAction("Index", returnUrl);
+        }
+
+        public ActionResult Index(string returnurl)
+        {
+            return View(new CarrinhoViewModel
+            {
+                Carrinho = ObterCarrinho(),
+                ReturnUrl = returnurl
+            });
+        }
+    }
+}
---
+++ CyberPizza.LojaVirtual.Web/Models/CarrinhoViewModel.cs
@@ -0,0 +1,11 @@
+﻿using CyberPizza.LojaVirtual.Dominio.Entidades;
+
+namespace CyberPizza.LojaVirtual.Web.Models
+{
+    public class CarrinhoViewModel
+    {
+        public Carrinho Carrinho { get; set; }
+        public string ReturnUrl { get; set; }
+
+    }
+}
